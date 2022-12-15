03abc90a1fba55043f1fc77192c8e21379d6d6d2
Daniel Mellum <daniel.mellum@grin.co>
2022-12-15 11:13:23 -0500

    github actions why you no run?

--- docker/php8/entrypoint.sh
+++ docker/php8/entrypoint.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
 export DEBIAN_FRONTEND=noninteractive
-bash <(cat /etc/os-release; echo 'echo ${VERSION/*, /}')
 php -v
-cd /var/testing &&
-composer install --no-interaction &&
-dockerize -wait tcp://mariadb:3306 -timeout 1m &&
-./vendor/bin/phpunit --coverage-clover coverage.xml &&
-curl -Os https://uploader.codecov.io/latest/linux/codecov &&
-chmod +x codecov &&
-./codecov
+cd /var/testing && chmod +x ./runTests && ./runTests.sh
