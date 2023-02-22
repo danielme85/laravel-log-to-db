7585a0fc8a763e66e02e53559d2d1af3a27d81ab
Daniel Mellum <daniel.mellum@grin.co>
2023-02-22 17:59:08 -0500

    Merge remote-tracking branch 'origin/v4' into v4

--- buildDockerImages.sh
+++ buildDockerImages.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo "Building PHP8 image..." &&
-cd  ./docker/php8 && docker build . -t  ghcr.io/danielme85/lltdb-testbench:latest
+cd  ./docker/php8 && docker buildx build --platform linux/amd64,linux/arm64 --push . -t  ghcr.io/danielme85/lltdb-testbench:latest
